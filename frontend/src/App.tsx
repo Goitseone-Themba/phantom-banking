ec053d3ea970b3d0687e426a07a961caab1b48b3
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-06-06 20:00:26 +0200

    setup basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
-import { Businesses } from "./pages/admin/Businesses"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+
+import { Login } from "./pages/Login"
 import { AdminDashboard } from "./pages/admin/AdminDashboard"
+import { Businesses } from "./pages/admin/Businesses"
+
 import { Dashboard } from "./pages/merchant/Dashboard"
 import { Wallets } from "./pages/merchant/Wallets"
 import { WalletDetails } from "./pages/merchant/WalletDetails"
@@ -9,11 +13,23 @@ import { Transactions } from "./pages/merchant/Transactions"
 
 function App() {
 
-  return (
-    <>
-        <Transactions/>
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+
+                <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/admin/businesses" element={<Businesses />} />
+
+                <Route path="/merchant/dashboard" element={<Dashboard />} />
+                <Route path="/merchant/wallets" element={<Wallets/>} />
+                <Route path="/merchant/walletdetails" element={<WalletDetails />} />
+                <Route path="/merchant/creditwallet" element={<CreditWallet />} />
+                <Route path="/merchant/debitwallet" element={<DebitWallet />} />
+                <Route path="/merchant/transactions" element={<Transactions />} />
+            </Routes>
+        </Router>
+    )
 }
 
 export default App
