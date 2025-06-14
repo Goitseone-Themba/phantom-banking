b564c376f4642c02ab130e50f8c2661120473e45
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-06-14 22:28:42 +0200

    feat: auth context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/Login";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import { MerchantHome } from "./pages/MerchantHome";
 import { ForgotPassword } from "./pages/ForgotPassword";
 import { SignUp } from "./pages/SignUp";
 import { ProtectedRoute } from "./context/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
+import { MerchantHome } from "./pages/MerchantHome";
+import { CustomerHome } from "./pages/CustomerHome";
+import { AdminHome } from "./pages/AdminHome";
 function App() {
     return (
         <AuthProvider>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Login />} />
+                    <Route path="*" element={<MerchantHome />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<SignUp />} />
                     <Route path="/forgotpassword" element={<ForgotPassword />} />
-                    <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+                    <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
+                        <Route path="/admin" element={<AdminHome />} />
+                    </Route>
 
                     <Route element={<ProtectedRoute allowedRoles={["merchant"]} />}>
                         <Route path="/merchant" element={<MerchantHome />} />
                     </Route>
+
+                    <Route element={<ProtectedRoute allowedRoles={["customer"]} />}>
+                        <Route path="/merchant" element={<CustomerHome/>} />
+                    </Route>
                 </Routes>
-            </Router>
-        </AuthProvider>
+            </Router >
+        </AuthProvider >
     );
 }
 
